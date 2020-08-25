a4a9b32f605f44dbc48fa44a9c62efa55175646b
nathan <nathan@nbcaldwell.com>
2020-08-24 22:05:47 -0400

    Add bin loader

--- YKnyttLibConsole/Program.cs
+++ YKnyttLibConsole/Program.cs
@@ -12,18 +12,18 @@ namespace YKnyttLibConsole
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            KnyttWorld<bool> world = new KnyttWorld<bool>();
+            KnyttWorld world = new KnyttWorld();
             string world_ini = File.ReadAllText("Nifflas - The Machine/World.ini");
             world.loadWorldConfig(world_ini);
             var fs = File.Open("Nifflas - The Machine/Map.bin", FileMode.Open);
             world.loadWorldMap(fs);
             fs.Close();
 
-            KnyttWorldManager<bool, bool> manager = new KnyttWorldManager<bool, bool>();
+            KnyttWorldManager<bool> manager = new KnyttWorldManager<bool>();
             manager.addWorld(world, false);
 
 
-            var save = new KnyttSave<bool>(world, File.ReadAllText("Nifflas - The Machine/DefaultSavegame.ini"), 1);
+            var save = new KnyttSave(world, File.ReadAllText("Nifflas - The Machine/DefaultSavegame.ini"), 1);
             save.setPower(3, true);
             save.setArea(new KnyttPoint(123, 456));
             save.setAreaPosition(new KnyttPoint(1, 2));
@@ -39,6 +39,20 @@ namespace YKnyttLibConsole
             Console.WriteLine(world.Info.Difficulties[0]);
             Console.WriteLine(world.Info.Difficulties[1]);
             Console.WriteLine(world.getArea(new KnyttPoint(998, 1021)));
+
+            fs.Close();
+
+            fs = File.Open("Fubaka - The Column.knytt.bin", FileMode.Open);
+            KnyttBinWorldLoader bworld = new KnyttBinWorldLoader(fs);
+
+            Console.WriteLine(bworld.RootDirectory);
+
+            foreach (var fname in bworld.GetFileNames())
+            {
+                Console.WriteLine(fname);
+            }
+
+            //Console.WriteLine(bworld.GetFile("Ambiance/Ambi22.ogg"));
 
             Console.ReadKey();
         }
